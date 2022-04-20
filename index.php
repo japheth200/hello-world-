3cdf10a65e08bac9a9a962db345d747f69056576
japheth200 <103031098+japheth200@users.noreply.github.com>
2022-04-20 06:16:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>заданя--Hello word!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+    <img src="politech.png" alt="" class="logo">
+    <div  class="header">    
+        <p>Hello, world!<p>
+    </div>
+    <div></div>
+    </header>
+
+    <main>
+        <?php
+            echo 'Hello, world!';
+        ?>
+    </main>
+
+    <footer>
+        <div class="footer">
+            <div>
+            <p>синон жафет олу-воле 211-329</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
